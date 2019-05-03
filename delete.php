ae4d93a481b3b0b299221e295825b01b38762c25
KamehaMehdi <dahmammehdi@gmail.com>
2019-05-03 17:09:29 +0200

    testing without deleting draft

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
     $_POST['delete'] ? unlink('./scripts/' . $_POST['delete']) : null ;
-    $_POST['deleteDraft'] ? unlink('./draft/' . $_POST['deleteDraft'] . '.json') : null;
+    // $_POST['deleteDraft'] ? unlink('./draft/' . $_POST['deleteDraft'] . '.json') : null;
 ?>
     <form action="/" id="goHome"></form>
     <script>
