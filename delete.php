836bf37df44f47c97f8fa87cf37622bfe90639b1
KamehaMehdi <dahmammehdi@gmail.com>
2019-05-03 17:12:54 +0200

    testing without deleting draft

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
     $_POST['delete'] ? unlink('./scripts/' . $_POST['delete']) : null ;
     // $_POST['deleteDraft'] ? unlink('./draft/' . $_POST['deleteDraft'] . '.json') : null;
 
-
+echo "un test de tes morts";
 ?>
     <form action="/" id="goHome"></form>
     <script>
